54f812c8f9d9ec7a613796fb4678de208963ba5e
Brad Hezh <bradhezh@gmail.com>
2025-10-29 16:36:16 +1300

    supabase.sql: comments added

--- backend/src/models/supabase.sql
+++ backend/src/models/supabase.sql
@@ -1,4 +1,7 @@
 CREATE ROLE mikro_user WITH LOGIN PASSWORD 'password';
+-- the free tier service could be paused and then needs to be resumed, and after
+-- resuming, the password might need to be set again to be activated
+-- ALTER ROLE mikro_user WITH LOGIN PASSWORD 'password';
 
 GRANT USAGE, CREATE ON SCHEMA public TO mikro_user;
 
